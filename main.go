dc3dc9bda6ca4320fd58c85f7986f937eee859b8
Hiram J. Pérez <worg@linuxmail.org>
2014-06-10 16:41:04 -0500

    intial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at http://mozilla.org/MPL/2.0/.
+© 2014 Hiram J. Pérez
+**/
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var port = flag.String(`p`, `8080`, `the port on wich we're serving`)
+var path = flag.String(`path`, `.`, `path that will be served`)
+
+func main() {
+	flag.Parse()
+	servePort := (map[bool]string{true: envPort, false: *port})[envPort != ``]
+	//ternary[ish] operator babe!
+
+	servePort = fmt.Sprintf(":%v", servePort)
+
+	fmt.Println("Serving files in", *path, "on port ", servePort)
+	http.Handle("/", http.FileServer(http.Dir(*path)))
+	err := http.ListenAndServe(servePort, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+#goSimpleServer
+
+A go implementation of python's SimpleHTTPServer
+
+##Install:
+
+`go install github.com/worg/goSimpleServer`
+
+##Basic Usage
+Just run `goSimpleServer` to start serving files in the current directory on port `8080`
+
+###Flags
+Flag    | Default |  Behaviour
+--------|:-------:| ------
+`-p`    | `8080`  | Port<br>[may be overridden also with the environment variable PORT]
+`--path`| `.`     | Specifies the path to be served
+
+
+###License
+```
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at http://mozilla.org/MPL/2.0/.
+
+© 2014 Hiram J. Pérez
+```
